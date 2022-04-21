72f4614084ae28dcaf788cd38f42b06cbed1759b
adarsh-2 <prasadadarshkumar174@gmail.com>
2022-04-21 15:21:50 +0530

    new

---
+++ scripts/common.js
@@ -0,0 +1,12 @@
+// Search for the modals
+var signUpModal = document.getElementById("signUpModal");
+var signInModal = document.getElementById("signInModal");
+
+// Search for the button to open modals
+var signUpBtn  = document.getElementById("signUpBtn");
+var signInBtn  = document.getElementById("signInBtn");
+var signUpLink = document.getElementById("signUpLink");
+
+// Search the button to close modal
+var closeSignUp = document.getElementById("closeSignUp");
+var closeSignIn = document.getElementById("closeSignIn");
