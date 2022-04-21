8b08aa236345e1afaed4bd7cbfecf31dcf753286
adarsh-2 <prasadadarshkumar174@gmail.com>
2022-04-21 15:34:36 +0530

    new

--- scripts/common.js
+++ scripts/common.js
@@ -10,3 +10,21 @@ var signUpLink = document.getElementById("signUpLink");
 // Search the button to close modal
 var closeSignUp = document.getElementById("closeSignUp");
 var closeSignIn = document.getElementById("closeSignIn");
+
+//Open the modal
+openModal = modal => {
+    modal.style.display = "block";
+};
+
+//Close the modal
+closeModal = modal => {
+    modal.style.display = "none";
+};
+
+// Event to handle the click on Signin/signup buttons
+signInBtn.addEventListener("click", () => openModal(signInModal));
+signUpBtn.addEventListener("click", () => openModal(signUpModal));
+
+// Event to handle close button
+closeSignUp.addEventListener("click", () => closeModal(signUpModal));
+closeSignIn.addEventListener("click", () => closeModal(signInModal));
